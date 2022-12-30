8d297fa2b1a8f1575dfb32464d0cce4f65fff460
Sevag <sevag1990@gmail.com>
2022-12-30 19:58:54 +0100

    day12

--- src/main/java/day12/day12.java
+++ src/main/java/day12/day12.java
@@ -6,75 +6,77 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Collections.*;
 
 
 public class day12 {
 
     public static void main(String[] args) {
-        Point end = new Point();
-        Point currentPoint = new Point();
-        Map<Point, String> possibleMove;
+        final Point end = new Point();
+        final Point start = new Point();
+        final List<Point> startList = new ArrayList<>();
+        final List<Integer> paths = new ArrayList<>();
 
-        String[] order = "abcdefghijklmnopqrstuvwxyz".split("");
-        var counter = 0;
-        boolean found = false;
-        Set<Point> visited = new HashSet<>();
-
-        List<String> inputs = getStringList();
-        Map<Point, String> heightmap = new HashMap<>();
-        parseInput(currentPoint, end, inputs, heightmap);
+        List<Point> que;
+        Map<Point, Integer> pathCost;
 
 
-        while (!found) {
-            possibleMove = new HashMap<>();
-            var up = new Point(currentPoint.x, currentPoint.y - 1);
-            var down = new Point(currentPoint.x, currentPoint.y + 1);
-            var left = new Point(currentPoint.x - 1, currentPoint.y);
-            var right = new Point(currentPoint.x + 1, currentPoint.y);
-
-            if (order[counter].equals("z")) {
-                found = true;
-                continue;
-            }
-            if (heightmap.get(up) != null) {
-                possibleMove.put(new Point(up), heightmap.get(up));
-            }
+        final List<String> inputs = getStringList();
+        final Map<Point, Integer> heightmap = new HashMap<>();
+        parseInput(start, end, inputs, heightmap, startList, false);
 
 
-            if (heightmap.get(down) != null) {
-                possibleMove.put(new Point(down), heightmap.get(down));
-            }
-            if (heightmap.get(left) != null) {
-                possibleMove.put(new Point(left), heightmap.get(left));
-            }
-            if (heightmap.get(right) != null) {
+        for (Point startPoint : startList) {
+            que = new ArrayList<>();
+            pathCost = new HashMap<>();
 
-                possibleMove.put(new Point(right), heightmap.get(right));
-            }
+            que.add(startPoint);
+            pathCost.put(startPoint, 0);
 
+            while (que.size() > 0) {
+                final Point p = que.remove(0);
+                final Map<Point, Integer> possibleMove = new HashMap<>();
+                var up = new Point(p.x, p.y - 1);
+                var down = new Point(p.x, p.y + 1);
+                var left = new Point(p.x - 1, p.y);
+                var right = new Point(p.x + 1, p.y);
 
-            for (Map.Entry<Point, String> t : possibleMove.entrySet()) {
-                if (!visited.contains(t.getKey())) {
-
-                    if (order[counter].compareTo(t.getValue()) == 0) {
-                        visited.add(new Point(t.getKey()));
-                        currentPoint = new Point(t.getKey());
+                if (heightmap.get(up) != null && check(p, heightmap, up)) {
+                    possibleMove.put(new Point(up), heightmap.get(up));
+                }
+                if (heightmap.get(down) != null && check(p, heightmap, down)) {
+                    possibleMove.put(new Point(down), heightmap.get(down));
+                }
+                if (heightmap.get(left) != null && check(p, heightmap, left)) {
+                    possibleMove.put(new Point(left), heightmap.get(left));
+                }
+                if (heightmap.get(right) != null && check(p, heightmap, right)) {
+                    possibleMove.put(new Point(right), heightmap.get(right));
+                }
+                for (Map.Entry<Point, Integer> t : possibleMove.entrySet()) {
+                    if (p.equals(end)) {
+                        paths.add(pathCost.get(p));
                         break;
+                    }
+                    if (!pathCost.containsKey(t.getKey())) {
+                        pathCost.put(t.getKey(), pathCost.getOrDefault(p, 0) + 1);
+                        que.add(t.getKey());
                     }
                 }
             }
-            counter++;
         }
 
-        System.out.println(visited.size());
-
+        System.out.println("Result: " + min(paths));
     }
 
+    private static boolean check(Point currentPoint, Map<Point, Integer> heightmap, Point point) {
+        return heightmap.get(point) <= heightmap.get(currentPoint) ||
+                heightmap.get(point) == heightmap.get(currentPoint) + 1;
+    }
 
-    private static void parseInput(Point start, Point end, List<String> inputs, Map<Point, String> map) {
+    private static void parseInput(Point start, Point end, List<String> inputs, Map<Point, Integer> map, List<Point> startList, boolean part1) {
         int x;
         int y = 0;
 
@@ -83,12 +85,27 @@ public class day12 {
             for (String word : input.split("")) {
                 if (word.equals("S")) {
                     start.setLocation(new Point(x, y));
+                    map.put(new Point(x, y), 0);
+                    startList.add(new Point(x, y));
+                    x++;
+                    continue;
+                }
+                if (!part1) {
+                    if (word.equals("a")) {
+                        map.put(new Point(x, y), 0);
+                        startList.add(new Point(x, y));
+                        x++;
+                        continue;
+                    }
                 }
                 if (word.equals("E")) {
                     end.setLocation(new Point(x, y));
+                    map.put(new Point(x, y), 'z' - 'a');
+                    x++;
+                    continue;
                 }
 
-                map.put(new Point(x, y), word);
+                map.put(new Point(x, y), word.toLowerCase(Locale.ROOT).charAt(0) - 'a');
                 x++;
             }
             y++;
@@ -97,9 +114,7 @@ public class day12 {
 
 
     private static List<String> getStringList() {
-        try (Stream<String> stream = Files
-                .readAllLines(Paths.get("inputs", "day12.txt"))
-                .stream()) {
+        try (Stream<String> stream = Files.readAllLines(Paths.get("inputs", "day12.txt")).stream()) {
             return stream.toList();
         } catch (final IOException e) {
             e.printStackTrace();
