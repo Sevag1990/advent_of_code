e31f417daaa157fc375046d16994e25427d47d15
Sevag <sevag1990@gmail.com>
2023-01-17 21:21:23 +0100

    refactoring

--- javascript_2015/day3.js
+++ javascript_2015/day3.js
@@ -1,7 +1,38 @@
-run();
+runPart1();
+runPart2();
+
+function runPart1() {
+    const fs = require("fs");
+    const lines = fs
+        .readFileSync("./javascript_2015/inputs/day3.txt", { encoding: "utf-8" })
+        .split("")
+        .map(line => line.trim());
+
+    let grid = [0, 0];
+    let visited = new Set();
+    visited.add(`0x0`);
 
 
-function run() {
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i] == '^') {
+                grid = [grid[0], grid[1] - 1];
+        }
+
+        if (lines[i] == 'v') {
+                grid = [grid[0], grid[1] + 1];
+        }
+        if (lines[i] == '>') {
+                grid = [grid[0] + 1, grid[1]];
+        }
+        if (lines[i] == '<') {
+                grid = [grid[0] - 1, grid[1]];
+        }
+
+            visited.add(`${grid[0]}x${grid[1]}`);
+    }
+    console.log(`Part 1: ${visited.size}`);
+}
+function runPart2() {
     const fs = require("fs");
     const lines = fs
         .readFileSync("./javascript_2015/inputs/day3.txt", { encoding: "utf-8" })
@@ -12,9 +43,7 @@ function run() {
     let roboGrid = [0, 0];
     let visited = new Set();
     visited.add(`0x0`);
-    
-    let visitedPart2 = new Set();
-    visitedPart2.add(`0x0`);
+
 
     for (let i = 0; i < lines.length; i++) {
         if (lines[i] == '^') {
@@ -48,14 +77,14 @@ function run() {
                 roboGrid = [roboGrid[0] - 1, roboGrid[1]];
             }
         }
-        visited.add(`${grid[0]}x${grid[1]}`);
 
         if (i  % 2 === 0) {
-            visitedPart2.add(`${grid[0]}x${grid[1]}`);
+            visited.add(`${grid[0]}x${grid[1]}`);
         } else {
-            visitedPart2.add(`${roboGrid[0]}x${roboGrid[1]}`);
+            visited.add(`${roboGrid[0]}x${roboGrid[1]}`);
         }
     }
-    console.log(`Part 1: ${visited.size} houses`);
-    console.log(`Part 2: ${visitedPart2.size}`);
+    console.log(`Part 2: ${visited.size}`);
 }
+
+
