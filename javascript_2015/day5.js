51f167228b9b37513437458c453e603c90b8e8a9
Sevag Antranik <sevag1990@gmail.com>
2023-01-18 20:18:57 +0100

    day5 p2

--- javascript_2015/day5.js
+++ javascript_2015/day5.js
@@ -1,9 +1,8 @@
 
-runPart1();
-//runPart2();
+//runPart1();
+runPart2();
 
 function runPart1() {
-    var counter = 0;
     var niceStrings = 0;
 
     const fs = require("fs");
@@ -13,11 +12,11 @@ function runPart1() {
         .map(line => line.trim());
     const VOWELS = ['a', 'e', 'i', 'o', 'u'];
     const RESTRICTED_LETTERS = ['ab', 'cd', 'pq', 'xy'];
-    const DOUBLE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('').map(item => item + item);
+    const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('').map(item => item + item);
 
     for (let i = 0; i < lines.length; i++) {
         let word = lines[i].split("");
-        let prop1,prop2 = false;
+        let prop1, prop2 = false;
         let prop3 = true;
 
         for (let x = 0; x < word.length; x++) {
@@ -26,7 +25,7 @@ function runPart1() {
             }
         }
         for (let x = 0; x < word.length; x++) {
-            if (DOUBLE_LETTERS.includes(`${word[x]}${word[x + 1]}`)) {
+            if (LETTERS.includes(`${word[x]}${word[x + 1]}`)) {
                 prop2 = true;
             }
         }
@@ -35,18 +34,44 @@ function runPart1() {
         if (string_vowels.length >= 3) {
             prop1 = true;
         }
-            if(prop1 && prop2 && prop3){
-                niceStrings++;
-            }
+        if (prop1 && prop2 && prop3) {
+            niceStrings++;
         }
-        
+    }
+
     console.log(`Part 1: ${niceStrings}`);
 }
 
 
 function runPart2() {
+    var niceStrings = 0;
 
-    console.log(`Part 2: ${counter}`);
+    const fs = require("fs");
+
+    const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('').map(item => item + item);
+    const lines = fs
+        .readFileSync("./javascript_2015/inputs/day5.txt", { encoding: "utf-8" })
+        .split("\n")
+        .map(line => line.trim());
+
+
+    for (let i = 0; i < lines.length; i++) {
+        let word = lines[i].split("");
+        let prop1, prop2 = false;
+
+        for (let x = 0; x < word.length; x++) {
+            if (LETTERS.includes(`${word[x]}${word[x + 2]}`)) {
+                prop1 = true;
+            }
+        }
+        if (/([a-z]{2})[a-z]*\1/.test(lines[i])) {
+            prop2 = true;
+        }
+
+        if (prop1 && prop2) {
+            niceStrings++;
+        }
+    }
+
+    console.log(`Part 2: ${niceStrings}`);
 }
-
-
